f753d769eef4ca528b92155c79d986b82a9d9f6e
Florian Lentsch <office@florian-lentsch.at>
2017-10-01 16:51:01 +0200

    removed webrtc

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,7 +2,6 @@ import "reflect-metadata"; // <- required for AppModel
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 import {enableProdMode} from '@angular/core';
-import 'webrtc-adapter/out/adapter.js';
 
 enableProdMode();
 
