b75180a5dbaf0586ace9347b19e5c8e0cdc01fd6
Florian Lentsch <office@florian-lentsch.at>
2019-09-18 10:16:58 +0200

    Fix for move form sync

--- src/modal/product-entries/move-form/product-entry-move-form.ts
+++ src/modal/product-entries/move-form/product-entry-move-form.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Events, ModalController } from '@ionic/angular';
 import { ExpirySyncController } from 'src/app/app.expiry-sync-controller';
-import { ProductEntry, Location } from 'src/app/models';
+import { ProductEntry, Location, Article, ArticleImage } from 'src/app/models';
 import { UiHelper } from 'src/utils/ui-helper';
 import { ExpirySync } from 'src/app/app.expiry-sync';
 
@@ -58,11 +58,25 @@ export class ProductEntryMoveFormModal extends ExpirySyncController implements O
     const task = app.loadingStarted('Moving entries...');
     for (const entry of this.productEntries) {
       // Create a clone at the target location:
-      const copy = <ProductEntry>entry.clone();
-      copy.locationId = targetLocation.id;
-      copy.location = targetLocation;
-      copy.inSync = false;
-      await copy.save();
+      const entryCopy = <ProductEntry> entry.clone();
+      if (!entryCopy.article.barcode) {
+        const originalImages = <ArticleImage[]> await ArticleImage.all()
+          .filter('articleId', '=', entryCopy.article.id)
+          .list();
+        entryCopy.article = <Article> entryCopy.article.clone();
+        await entryCopy.article.save();
+        entryCopy.articleId = entryCopy.article.id;
+
+        for (const originalImage of originalImages) {
+          const imageCopy = <ArticleImage>originalImage.clone();
+          imageCopy.articleId = entryCopy.articleId;
+          await imageCopy.save();
+        }
+      }
+      entryCopy.locationId = targetLocation.id;
+      entryCopy.location = targetLocation;
+      entryCopy.inSync = false;
+      await entryCopy.save();
 
       // remove the old entry:
       await entry.markForDeletion();
@@ -70,7 +84,7 @@ export class ProductEntryMoveFormModal extends ExpirySyncController implements O
 
     app.entriesList.loadingAfterLocationSwitchDone = false;
     if (app.currentUser.loggedIn) {
-      await app.synchronize();
+      await app.synchronize(false, true);
     }
     this.events.publish('app:syncDone');
     app.loadingDone(task);
